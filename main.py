480e174c1145998e69f6bfb813338c9f775ce6c4
정지민 <108014449+stopmin@users.noreply.github.com>
2025-06-22 18:02:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ markdown_text = """
 ## 안녕하세요!
 부산대학교 정보컴퓨터공학부 22학번 정지민입니다.
 
+## 📄 Resume
+- [View My Resume on Rallit](https://www.rallit.com/resumes/65210@geemin0/%EC%A0%95%EC%A7%80%EB%AF%BC)
 
 ## GitHub Streak
 [![GitHub Streak](https://streak-stats.demolab.com?user=Stopmin&theme=onedark-duo)](https://git.io/streak-stats)
@@ -20,13 +22,11 @@ markdown_text = """
 """  # list of blog posts will be appended here
 
 for idx, feed in enumerate(RSS_FEED['entries']):
-    if idx >= MAX_POST:  # '>= MAX_POST' 사용하여 정확한 포스트 수를 맞춥니다.
+    if idx >= MAX_POST:
         break
     else:
         feed_date = feed['published_parsed']
-        # Markdown에서의 줄 바꿈 처리를 위해 라인 끝에 공백 두 개 추가
         markdown_text += f"- [{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']})  \n"
         
 with open("README.md", "w", encoding="utf-8") as f:
     f.write(markdown_text)
-
